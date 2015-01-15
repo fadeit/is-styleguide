3282b345d16888821d1c562b7b5138eaca4ed268
Dan Mindru <mindrudan@gmail.com>
2015-01-15 13:11:01 +0100

    added gulp plugins to watch files and compress css, moved index to root, re-organized asset and source files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    LessPluginCleanCSS = require("less-plugin-clean-css"),
+    cleancss = new LessPluginCleanCSS({advanced: true});
+
+gulp.task('less', function(){
+  gulp.src('./src/style-guide.less')
+      .pipe(less({
+        plugins: [cleancss]
+      }))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('this-styleguide', function(){
+  gulp.src('./assets/this-styleguide.less')
+      .pipe(less({
+        plugins: [cleancss]
+      }))
+      .pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/**/*.less', ['default']);
+  gulp.watch('./assets/**/*.less', ['default']);
+});
+
+gulp.task('default', ['less', 'watch']);
