3508cfd9a408bf0c793f08a3ffce35897abec4fd
Dan Mindru <mindrudan@gmail.com>
2015-01-19 16:44:38 +0100

    making icon-fonts with Gulp and generating CSS with some tricks, re-organized files a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp'),
+    fs = require('fs'),
     watch = require('gulp-watch'),
     less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    iconfont = require('gulp-iconfont'),
     LessPluginCleanCSS = require("less-plugin-clean-css"),
     cleancss = new LessPluginCleanCSS({
       advanced: true,
@@ -8,10 +11,14 @@ var gulp = require('gulp'),
     });
 
 gulp.task('less', function(){
-  gulp.src('./src/style-guide.less')
+  gulp.src([
+        './src/style-guide.less',
+        './assets/generated-webfont.less'
+      ])
       .pipe(less({
         plugins: [cleancss]
       }))
+      .pipe(concat('ols-style-guide.css'))
       .pipe(gulp.dest('./dist/'));
 });
 
@@ -23,24 +30,64 @@ gulp.task('this-styleguide', function(){
       .pipe(gulp.dest('./assets/'));
 });
 
-gulp.task('copy-styleguide-assets', function(){
+gulp.task('copy-styleguide-fonts', function(){
   gulp.src([
         './assets/**/*-webfont.eot',
         './assets/**/*-webfont.svg',
         './assets/**/*-webfont.ttf',
         './assets/**/*-webfont.woff'
       ])
-      .pipe(gulp.dest('./dist/styleguide-assets/'));
+      .pipe(gulp.dest('./dist/styleguide-assets/fonts/'));
+});
+
+gulp.task('copy-styleguide-images', function(){
+  gulp.src([
+        './assets/images/**/*'
+      ])
+      .pipe(gulp.dest('./dist/styleguide-assets/images/'));
+});
+
+gulp.task('icon-fonts', function(){
+  gulp.src(['./assets/icon-fonts/*.svg'])
+      .pipe(iconfont({
+        fontName: 'ols',
+        appendCodepoints: true,
+        normalize: true
+      }))
+      .on('codepoints', function(codepoints, options) {
+        var i;
+        var fileContents = '';
+
+        for(i = 0; i <= codepoints.length - 1; i++){
+          fileContents +=
+            '.' + codepoints[i].name + ' {\n' +
+              '  content: "\\E00' + (i + 1) + '";\n' +
+            '}\n\n';
+        }
+
+        fs.writeFile('./assets/generated-webfont.less', fileContents);
+      })
+      .pipe(gulp.dest('./dist/styleguide-assets/fonts/'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./src/**/*.less', ['less']);
-  gulp.watch('./assets/**/*.less', ['this-styleguide']);
+  gulp.watch('./src/**/*.less', ['dist']);
+  gulp.watch('./assets/**/*.less', ['example-page']);
 });
 
 gulp.task('default', [
-  'less',
-  'this-styleguide',
-  'copy-styleguide-assets',
+  'example-page',
+  'dist',
   'watch'
+]);
+
+gulp.task('example-page', [
+  'this-styleguide'
+]);
+
+gulp.task('dist', [
+  'copy-styleguide-fonts',
+  'copy-styleguide-images',
+  'icon-fonts',
+  'less'
 ]);
