5c6aa31dbd7dc71eb723e2fd48e94dbc4d4d9b43
Dan Mindru <mindrudan@gmail.com>
2015-01-26 19:33:05 +0100

    added basic inputs, fixed large search bar

--- assets/this-styleguide.js
+++ assets/this-styleguide.js
@@ -1,99 +1,162 @@
-//toggle code
-(function codeToggler(){
-  var i,
-      codeBlocks = document.querySelectorAll('.code-toolbar');
+(function styleGuideApp(){
+  //toggle code blocks
+  (function codeToggler(){
+    var i,
+        codeBlocks = document.querySelectorAll('.code-toolbar');
 
-  for(i = 0; i<= codeBlocks.length-1; i++){
-    codeBlocks[i].addEventListener('click', expandCode);
-  }
-
-  function expandCode(e){
-    var className = new RegExp(e.target.className);
-    var pre = e.target;
-
-    if(!className.test('toggled-code') && e.target.tagName === 'PRE'){
-      pre.className = pre.className + ' toggled-code';
-
-      var toolbar = pre.querySelector('.toolbar');
-      var collapse_button = toolbar.querySelector('.collapse-code');
-
-      collapse_button.addEventListener('click', collapseCode);
+    for(i = 0; i<= codeBlocks.length-1; i++){
+      codeBlocks[i].addEventListener('click', expandCode);
     }
 
-    function collapseCode(e){
-      pre.className = pre.className.replace(' toggled-code', '');
-    }
-  }
-})();
+    function expandCode(e){
+      var pre = e.target;
 
-//generate ToC
-(function generateTableOfContents(){
-  var i,
-      toc_list,
-      toc_list_item,
-      toc_anchor,
-      toc_last_tag,
-      toc_last_list,
-      toc_nested_list_item,
-      toc_container = document.querySelector("#table-of-contents"),
-      toc_items = document.querySelectorAll('.toc-heading');
+      if(!hasClass(pre, 'toggled-code') && e.target.tagName === 'PRE'){
+        addClass(pre, 'toggled-code');
 
-  for(i = 0; i <= toc_items.length - 1; i++) {
-    toc_list_item = document.createElement('li');
-    toc_anchor = document.createElement('a');
-    toc_anchor.href = "#"+toc_items[i].id;;
-    toc_anchor.innerHTML = toc_items[i].innerHTML;
+        var toolbar = pre.querySelector('.toolbar');
+        var collapse_button = toolbar.querySelector('.collapse-code');
 
-    //append anchor to list item
-    toc_list_item.appendChild(toc_anchor);
-
-    if(toc_last_tag !== toc_items[i].tagName){
-      //append list
-      if(toc_last_list){
-        toc_nested_list_item.appendChild(toc_last_list)
-        toc_container.appendChild(toc_nested_list_item);
+        collapse_button.addEventListener('click', collapseCode);
       }
 
-      toc_nested_list_item = document.createElement('li');
-      toc_last_tag = toc_items[i].tagName;
-      toc_last_list = document.createElement('ul');
-      toc_last_list.className = "toc-list-" + toc_items[i].tagName.toLowerCase();
+      function collapseCode(e){
+        removeClass(pre, 'toggled-code');
+      }
+    }
+  })();
+
+  //generate ToC
+  (function generateTableOfContents(){
+    var i,
+        tocList,
+        tocListItem,
+        tocAnchor,
+        tocLastTag,
+        tocLastList,
+        tocNestedListItem,
+        tocContainer = document.querySelector("#table-of-contents"),
+        tocItems = document.querySelectorAll('.toc-heading');
+
+    for(i = 0; i <= tocItems.length - 1; i++) {
+      tocListItem = document.createElement('li');
+      tocAnchor = document.createElement('a');
+      tocAnchor.href = "#"+tocItems[i].id;;
+      tocAnchor.innerHTML = tocItems[i].innerHTML;
+
+      //append anchor to list item
+      tocListItem.appendChild(tocAnchor);
+
+      if(tocLastTag !== tocItems[i].tagName){
+        //append list
+        if(tocLastList){
+          tocNestedListItem.appendChild(tocLastList)
+          tocContainer.appendChild(tocNestedListItem);
+        }
+
+        tocNestedListItem = document.createElement('li');
+        tocLastTag = tocItems[i].tagName;
+        tocLastList = document.createElement('ul');
+        addClass(tocLastList, 'toc-list-'+tocItems[i].tagName.toLowerCase());
+      }
+
+      tocLastList.appendChild(tocListItem);
+    }
+  })();
+
+  //tocScroll
+  (function TocScroll(){
+    var introSize = document.getElementsByClassName('intro')[0].offsetHeight,
+        tocContainer = document.querySelector('#toc-container'),
+        scrollGuard = false;
+
+    window.onscroll = handleScrollChange;
+
+    function handleScrollChange(){
+      var scrollPosition = getScrollTop();
+
+      if(scrollPosition >= introSize && !scrollGuard){
+        addClass(tocContainer, 'fixed-table');
+        scrollGuard = true;
+      } else if(scrollPosition < introSize && scrollGuard){
+        removeClass(tocContainer, 'fixed-table');
+        scrollGuard = false;
+      }
     }
 
-    toc_last_list.appendChild(toc_list_item);
+    function getScrollTop(){
+      if(typeof pageYOffset != 'undefined'){
+        return pageYOffset;
+      }
+      else{
+        var B= document.body; //IE 'quirks'
+        var D= document.documentElement;
+        D= (D.offsetHeight)? D: B;
+        return D.scrollTop;
+      }
+    }
+  })();
+
+  //focus text input
+  (function focusTextInput(){
+    var i,
+        j,
+        dynamic_input_rows = document.querySelectorAll('.dynamic-input-row')
+
+    for(i = 0; i <= dynamic_input_rows.length - 1; i++){
+      addInputListenersTo(dynamic_input_rows[i].querySelectorAll('input[type], textarea'));
+    }
+
+    function addInputListenersTo(inputs){
+      if(inputs){
+        for(j = 0; j <= inputs.length - 1; j++){
+          inputs[j].addEventListener('focus', dynamicInputFocused, true);
+          inputs[j].addEventListener('blur', dynamicInputFocused, true);
+        }
+      }
+    }
+
+    function dynamicInputFocused(e){
+      if(!hasClass(this.parentNode, 'input-focused')){
+        addClass(this.parentNode, 'input-focused');
+      } else {
+        if(!this.value){
+          removeClass(this.parentNode, 'input-focused');
+        }
+      }
+    }
+  })();
+
+
+  /*
+   * Helper methods
+   * Should simplify tasks.
+   *
+   */
+
+  //Check if a element has a class
+  function hasClass(haystack, needle){
+    var fullClassName = haystack.className;
+    var classRegExp = new RegExp(needle);
+    return classRegExp.test(fullClassName);
   }
-})();
 
-//tocScroll
-(function TocScroll(){
-  var intro_size = document.getElementsByClassName('intro')[0].offsetHeight,
-      toc_container = document.querySelector('#toc-container'),
-      scroll_guard = false;
+  //Remove a class from an element
+  function removeClass(targetElement, classToRemove){
+    var initialClass = targetElement.className;
 
-  window.onscroll = handleScrollChange;
+    targetElement.className = targetElement.className.replace(' ' + classToRemove, '');
 
-  function handleScrollChange(){
-    //console.log(intro_size);
-    var scroll_position = getScrollTop();
-
-    if(scroll_position >= intro_size && !scroll_guard){
-      toc_container.className = toc_container.className + 'fixed-table';
-      scroll_guard = true;
-    } else if(scroll_position < intro_size && scroll_guard){
-      toc_container.className = toc_container.className.replace('fixed-table', '');
-      scroll_guard = false;
+    //try without space
+    if(targetElement.className === initialClass){
+      targetElement.className = targetElement.className.replace(classToRemove, '');
     }
   }
 
-  function getScrollTop(){
-    if(typeof pageYOffset != 'undefined'){
-      return pageYOffset;
-    }
-    else{
-      var B= document.body; //IE 'quirks'
-      var D= document.documentElement;
-      D= (D.offsetHeight)? D: B;
-      return D.scrollTop;
-    }
+  //add a class to a element
+  function addClass(targetElement, newClass){
+    var separator = (!targetElement.className) ? '' : ' ';
+
+    targetElement.className = targetElement.className + separator + newClass;
   }
 })();
